0a1e54089043b02fce431ac0215921679f579e3b
Huy <huy@hhoang.dev>
2022-02-21 16:34:48 +0100

    weekly report

--- toggl/main_pandas.py
+++ toggl/main_pandas.py
@@ -4,92 +4,73 @@ import json
 from dataclasses import dataclass
 
 import pandas as pd
+pd.options.display.width = 0
 
 from helpers import parse_time
 
-@dataclass
-class TogglEntry:
-	description: str
-	start: datetime
-	end: datetime
-	dur: int
-	client: str
-	project: str
-
-	def __init__(self, entry: dict):
-		self.description = entry['description']
-		self.start = parse_time(entry['start'])
-		self.end = parse_time(entry['end'])
-		self.dur = int(entry['dur'] / 1000)
-		self.client = entry['client']
-		self.project = entry['project']
-
-def duration_report(data: list[TogglEntry]):
-	report = {}
-	for d in data:
-		if d.client not in report:
-			report[d.client] = 0
-
-		report[d.client] += d.dur
-		
-	report = {k: round(v/3600, 2) for k,v in report.items()}
-
-	return report
-
-# def filter_by_day(data: list[TogglEntry], day: datetime.date):
-# 	for 
-
-def sum_duration(data: list[TogglEntry]):
-	duration = 0
-	for d in data:
-		duration += d.dur
-	return duration
 
 def get_last_week(df: pd.DataFrame):
+	df['start_date'] = df['start'].apply(lambda x: x.date())
+	df['end_date'] = df['end'].apply(lambda x: x.date())
 
-	now = datetime.datetime.now()
-	end_date = datetime.datetime.combine(now, datetime.time.max)
-	tmp = datetime.datetime.combine(end_date, datetime.time.min)
-	start_date = tmp - datetime.timedelta(days=6)
+	end_date = datetime.datetime.now().date()
+	start_date = end_date - datetime.timedelta(days=7)
 
-	mask = (df['start'] >= start_date) & (df['end'] < end_date)
+	mask = (df['start_date'] >= start_date) & (df['end_date'] < end_date)
 	filtered = df.loc[mask]
 	return filtered
 
-def get_group_durations(df, group_name: str):
-	grouped = df.groupby(group_name)
-	total_dur = 0
-	dur_seconds = {}
 
-	for group in grouped.groups:
-		dur = sum(grouped.get_group(group)['dur'])
-		dur_seconds[group] = dur
-		total_dur += dur
+def get_group_durations(df: pd.DataFrame, group_name: str):
+	# grouped = df.groupby(['end_date', group_name])
+	date_group = df.groupby('start_date')
+	week = pd.DataFrame()
+	week['day_total'] = 0
 
-	dur_percentage = {k: round((v/ total_dur) * 100, 4) for k, v in dur_seconds.items()}
+	for date in date_group.groups:
+		day = date_group.get_group(date)
+		grouped = day.groupby(group_name)
 
-	return dur_seconds, dur_percentage
+		total_day_dur = 0
+		for group in grouped.groups:
+			dur = sum(grouped.get_group(group)['dur'])
+			total_day_dur += dur
+			week.loc[date, group] = dur
+
+		week.loc[date, 'day_total'] = total_day_dur
+
+	week.fillna(0, inplace=True)
+
+	# add total column
+	week.loc['total'] = 0
+	for col in week.columns:
+		week.loc['total', col] = sum(week[col])
+
+	# create df with percentages
+	week_percentages = week.copy()
+	week_percentages.drop('day_total', axis=1, inplace=True)
+	for col in week_percentages.columns:
+		week_percentages[col] = round(week[col] / week["day_total"] * 100, 2)
+
+	return week, week_percentages
 
 
 def main():
-	with open('./data/2022-02-01.json') as f:
-		data = json.load(f)
-
 	df = pd.read_json('./data/2022-02-01.json')
 	rows_to_delete = ['id', 'pid', 'tid', 'uid', 'billable', 'is_billable', 'cur',
 										'tags', 'task', 'project_color']
 	df.drop(rows_to_delete, axis=1, inplace=True)
-	# df['start'] = df['start'].apply(lambda x: x.split('+')[0])
-	# df['start'] = pd.to_datetime(df['start'], format='%Y-%m-%dT%H:%M:%S')
 	df['start'] = df['start'].apply(parse_time)
 	df['end'] = df['end'].apply(parse_time)
 	df['dur'] = df['dur'].apply(lambda x: int(x/1000))
 
 	last_week = get_last_week(df)
 
-	dur_seconds, dur_percentage = get_group_durations(df, 'client')
-	for k,v in dur_percentage.items():
-		print(k, v)
+	week, week_percentages = get_group_durations(last_week, 'client')
+	week_hours = week.applymap(lambda x: round(x/3600, 2))
+	print(week_hours.head(10))
+	print()
+	print(week_percentages.head(10))
 
 
 if __name__ == '__main__':
